932c81b2c02032604c961eba1f34d3190436ce7e
Uzomzii <uzomaknwachukwu@gmail.com>
2025-05-22 11:34:40 +0100

    fixing starting files 2

--- web_test/settings.py
+++ web_test/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4(@+k#c&50_b&4trvs&m$)+7fav#6ydbhpa5k6lhs@@ije5y^k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
